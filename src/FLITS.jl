50036c805167d8dc962d13112c53d9e7bee6882b
kmaitreys <km.maitrey@gmail.com>
2025-02-10 10:48:21 +0530

    add modules

--- src/FLITS.jl
+++ src/FLITS.jl
@@ -1,5 +1,20 @@
 module FLITS
 
-greet() = print("Hello World!")
+include("Delaunay.jl")
+include("FITS.jl")
+include("InOut.jl")
+include("LTE.jl")
+include("Paths.jl")
+include("Reader.jl")
+include("RTContinum.jl")
+include("RTLines.jl")
+include("Structure.jl")
+include("Utils.jl")
+
+using .Delaunay
+
+
+
+
 
 end # module FLITS
